9050dcaa926a2ff45d24e6ac14bbaa062aeb8072
saraqgdeveloper <47433712+saraqgdeveloper@users.noreply.github.com>
2019-02-10 23:19:05 +0100

    Ejercicio de uso de un método de extensión.

---
+++ MetodoExtension/MetodoExtension/Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Clase que implementa un método de extensión, para "agregar" dentro de este proyecto el método EsPositivo, a la clase Integer
+//La clase será estática, ya que todos sus atributos y métodos serán estáticos
+namespace MetodoExtension
+{
+    static class Extension
+    {
+        //Método que "agrega" la funcionalidad, a la clase Integer, de indicar si el entero pasado por parámetro es positivo
+        public static bool EsPositivo(this int num1)//Mediante this, indicamos que estamos extendiendo a la clase int
+        {
+            if ((num1 > 0) || (num1==0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MetodoExtension/MetodoExtension/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MetodoExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Al ser estática la clase Extension, no podemos instanciarla. Veremos que el método de extensión
+            //está integrado en la clase Integer, a efectos de este proyecto, por lo que podremos usarlo sin problemas.
+
+            int numero1 = 15;
+
+            bool signo = numero1.EsPositivo();
+
+            if (signo)
+            {
+                Console.WriteLine("El número {0} es un número positivo", numero1);
+            }
+            else
+            {
+                Console.WriteLine("El número {0} es un número negativo", numero1);
+            }
+
+        }
+    }
+}
